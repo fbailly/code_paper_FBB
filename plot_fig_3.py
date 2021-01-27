b06b1de5977daf171d002ba1abcf55ce6e6ed4ac
fbailly <fbailly@laas.fr>
2021-01-27 16:39:14 +0100

    rename plot figure scripts

--- plot_fig_3.py
+++ plot_fig_3.py
@@ -1,130 +1,65 @@
-import seaborn
 import matplotlib.pyplot as plt
-import pickle
-from utils import *
-import matplotlib.ticker as ticker
+import numpy as np
+import scipy.io as sio
+import seaborn
 
-# Variables of the problem
-biorbd_model = biorbd.Model("arm_wt_rot_scap.bioMod")
-T = 8
-start_delay = 25  # Start movement after 25 first nodes
-Ns = 800 - start_delay
-T = T * (Ns) / 800
-final_offset = 5  # Ignore last 5 node when plot EMG
-tau_init = 0
-muscle_init = 0.5
-nbMT = biorbd_model.nbMuscleTotal()
-nbQ = biorbd_model.nbQ()
-nbGT = 0
-nb_try = 1
-co = "2"
-motion = "REACH2"
-muscles_names = [
-    "Pec sternal",
-    "Pec ribs",
-    "Lat thoracic",
-    "Lat lumbar",
-    "Lat iliac",
-    "Delt posterior",
-    "Tri long",
-    "Tri lat",
-    "Tri med",
-    "Brachial",
-    "Brachioradial",
-    "Pec clavicular",
-    "Delt anterior",
-    "Delt middle",
-    "Supraspin",
-    "Infraspin",
-    "Subscap",
-    "Bic long",
-    "Bic short",
-]
+# Configuration of problem
+use_torque = False
+use_noise = False
 
-# Noises informations
-marker_noise_lvl = [0, 0.002, 0.005, 0.01]
-EMG_noise_lvl = [0, 1, 1.5, 2]
-EMG_noise = np.ndarray((nb_try, len(EMG_noise_lvl), nbMT, Ns + 1))
-marker_noise = np.ndarray((nb_try, len(marker_noise_lvl), 3, biorbd_model.nbMarkers(), Ns + 1))
+nb_try = 100  # Number of try used to generate data
 
-# Get reference movement
-with open(f"solutions/sim_ac_8000ms_800sn_{motion}_co_level_{co}.bob", "rb") as file:
-    data = pickle.load(file)
-states = data["data"][0]
-controls = data["data"][1]
-q_ref = states["q"][:, start_delay:]
-dq_ref = states["q_dot"][:, start_delay:]
-a_ref = states["muscles"][:, start_delay:]
-u_ref = controls["muscles"][:, start_delay:]
-w_tau = "tau" in controls.keys()  # Look if there are residual torque in the reference movement
-if w_tau:
-    tau = controls["tau"]
-else:
-    tau = np.zeros((nbGT, Ns + 1))
+# Get data
+matcontent = sio.loadmat("solutions/stats_rt_activation_drivenTrue.mat")
+err_ac = matcontent["err_tries"]
+err_ac_mhe = err_ac[:-1, :].reshape(-1, nb_try, 10)
+err_ac_full = err_ac[-1, :].reshape(1, 10)
+err_mean_ac = np.mean(err_ac_mhe, axis=1)
+err_std_ac = np.std(err_ac_mhe, axis=1)
+err_mean_ac_full = np.concatenate((err_mean_ac, err_ac_full))
+Nmhe_ac = err_mean_ac_full[:, 0]
+ratio_ac = err_mean_ac_full[:-1, 1]
+time_tot_ac = err_mean_ac_full[:, 2]
+time_ac = err_mean_ac_full[:, 3]
+time_std_ac = err_std_ac[:, 3]
+time_mean_ac = err_mean_ac[:, 3]
+nb_try = 100
 
-# Generate EMG for all noise levels. Loop for all trials, markers and EMG noise
-for tries in range(nb_try):
-    for marker_lvl in range(1):
-        for EMG_lvl in range(len(EMG_noise_lvl)):
-            get_markers = markers_fun(biorbd_model)
-            markers_target = np.zeros((3, biorbd_model.nbMarkers(), Ns + 1))
-            for i in range(Ns + 1):
-                markers_target[:, :, i] = get_markers(q_ref[:, i])
-            EMG_noise[tries, EMG_lvl, :, :] = u_ref
+matcontent = sio.loadmat("solutions/stats_rt_activation_drivenFalse.mat")
+err_ex = matcontent["err_tries"]
+err_ex_mhe = err_ex[:-1, :].reshape(-1, nb_try, 10)
+err_ex_full = err_ex[-1, :].reshape(1, 10)
+err_mean_ex = np.mean(err_ex_mhe, axis=1)
+err_std_ex = np.std(err_ex_mhe, axis=1)
+err_mean_ex_full = np.concatenate((err_mean_ex, err_ex_full))
+Nmhe_ex = err_mean_ex_full[:, 0]
+ratio_ex = err_mean_ex_full[:-1, 1]
+time_tot_ex = err_mean_ex_full[:, 2]
+time_ex = err_mean_ex_full[:, 3]
+time_std_ex = err_std_ex[:, 3]
+time_mean_ex = err_mean_ex[:, 3]
 
-            x_ref = np.concatenate((q_ref, dq_ref, a_ref))
-            if marker_lvl != 0:
-                marker_noise[tries, marker_lvl, :, :, :] = generate_noise(
-                    biorbd_model, q_ref, u_ref, marker_noise_lvl[marker_lvl], EMG_noise_lvl[EMG_lvl]
-                )[0]
-
-            if EMG_lvl != 0:
-                EMG_noise[tries, EMG_lvl, :, :] = generate_noise(
-                    biorbd_model, q_ref, u_ref, marker_noise_lvl[marker_lvl], EMG_noise_lvl[EMG_lvl]
-                )[1]
-
-# Plot EMG for all levels of noises
-t = np.linspace(0, T, Ns + 1 - final_offset)
+# Plot Frequency figure fonction of windows size
 seaborn.set_style("whitegrid")
 seaborn.color_palette()
-
-mean_emg = np.real(EMG_noise).mean(axis=0)
-std_emg = np.real(EMG_noise).std(axis=0)
-minors = np.linspace(0, 1, 2)
-fig = plt.figure("Muscles controls")
-plt.gcf().subplots_adjust(left=0.06, right=0.99, wspace=0.25, hspace=0.2)
-plt.gca().yaxis.set_major_formatter(ticker.StrMethodFormatter("{x:,.3f}"))
-plt.gca().yaxis.set_major_locator(ticker.FixedLocator(minors))
-emg_lvl = ["low", "mid", "high"]
-for i in range(biorbd_model.nbMuscles()):
-    fig = plt.subplot(4, 5, i + 1)
-    if i in [14, 15, 16, 17, 18]:
-        plt.xlabel("Time(s)")
-        plt.gca().yaxis.set_major_formatter(ticker.StrMethodFormatter("{x:,.3f}"))
-    else:
-        fig.set_xticklabels([])
-    if i in [0, 5, 10, 15]:
-        plt.ylabel("EMG to track")
-        plt.gca().yaxis.set_major_formatter(ticker.StrMethodFormatter("{x:,.3f}"))
-
-    if i == 18:
-        for k in range(1, len(EMG_noise_lvl)):
-            plt.plot(t, mean_emg[k, i, :-final_offset], label=f"EMG noise level: {emg_lvl[k-1]}")
-        plt.gca().yaxis.set_major_formatter(ticker.StrMethodFormatter("{x:,.3f}"))
-        # plt.gca().yaxis.set_major_locator(ticker.MultipleLocator(0.5))
-    else:
-        for k in range(1, len(EMG_noise_lvl)):
-            plt.plot(t, mean_emg[k, i, :-final_offset], label=f"EMG noise level: {emg_lvl[k-1]}")
-        plt.gca().yaxis.set_major_formatter(ticker.StrMethodFormatter("{x:,.3f}"))
-    plt.title(muscles_names[i])
-
-for i in range(biorbd_model.nbMuscles()):
-    fig = plt.subplot(4, 5, i + 1)
-    if i == 18:
-        for k in range(0, 1):
-            plt.plot(t, mean_emg[k, i, :-final_offset], "red", label=f"Reference")
-    else:
-        for k in range(0, 1):
-            plt.plot(t, mean_emg[k, i, :-final_offset], "red", label=f"Reference")
-plt.legend(bbox_to_anchor=(1.05, 0.80), loc="upper left", frameon=False)
+lw = 3  # Line width
+fig = plt.subplot()
+fig.plot(1 / time_mean_ac, lw=lw, label="activation driven")
+fig.plot(1 / time_mean_ex, lw=lw, label="excitation driven")
+fig.plot(
+    np.arange(len(Nmhe_ac) - 1), np.tile(1 / 0.075, (len(Nmhe_ac) - 1, 1)), "--", lw=lw, label="biofeedback standard"
+)
+fig.fill_between(
+    range(len(time_mean_ac)), 1 / (time_mean_ac + time_std_ac), 1 / (time_mean_ac - time_std_ac), alpha=0.2
+)
+fig.fill_between(
+    range(len(time_mean_ex)), 1 / (time_mean_ex + time_std_ex), 1 / (time_mean_ex - time_std_ex), alpha=0.2
+)
+fig.set_xticks(range(len(Nmhe_ac) - 1))
+plt.xticks(fontsize=12)
+plt.yticks(fontsize=12)
+fig.set_xticklabels(range(int(Nmhe_ac[0]), int(Nmhe_ac[-2] + 1)))
+plt.legend(fontsize=14)
+plt.ylabel("Freq. (Hz)", fontsize=14)
+plt.xlabel("Size of MHE window", fontsize=14)
 plt.show()
--- plot_fig_4.py
+++ plot_fig_4.py
@@ -2,15 +2,17 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy.io as sio
 import seaborn
+import matplotlib.ticker as ticker
 
-# Configuration of problem
+# configuration of plot
 use_torque = False
 use_noise = False
+use_log = True  # If plot logarithmic (base 10) error
 
-nb_try = 100  # Number of try used to generate data
+nb_try = 1  # Number of try used to generate data
 
 # Get data
-matcontent = sio.loadmat("solutions/stats_rt_activation_drivenTrue.mat")
+matcontent = sio.loadmat("solutions/stats_rt_test_activation_drivenTrue.mat")
 err_ac = matcontent["err_tries"]
 err_ac_mhe = err_ac[:-1, :].reshape(-1, nb_try, 10)
 err_ac_full = err_ac[-1, :].reshape(1, 10)
@@ -18,48 +20,154 @@ err_mean_ac = np.mean(err_ac_mhe, axis=1)
 err_std_ac = np.std(err_ac_mhe, axis=1)
 err_mean_ac_full = np.concatenate((err_mean_ac, err_ac_full))
 Nmhe_ac = err_mean_ac_full[:, 0]
-ratio_ac = err_mean_ac_full[:-1, 1]
+ratio = err_mean_ac_full[:, 1]
 time_tot_ac = err_mean_ac_full[:, 2]
 time_ac = err_mean_ac_full[:, 3]
 time_std_ac = err_std_ac[:, 3]
 time_mean_ac = err_mean_ac[:, 3]
-nb_try = 100
+if use_log:
+    err_q_ac = np.log10(err_mean_ac_full[:, 4] * 180 / np.pi)
+    err_dq_ac = np.log10(err_mean_ac_full[:, 5])
+    err_muscles_ac = np.log10(err_mean_ac_full[:, 7])
+    err_markers_ac = np.log10(err_mean_ac_full[:, 8])
+    err_force_ac = np.log10(err_mean_ac_full[:, 9])
+else:
+    err_q_ac = err_mean_ac_full[:, 4] * 180 / np.pi
+    err_dq_ac = err_mean_ac_full[:, 5]
+    err_muscles_ac = err_mean_ac_full[:, 7]
+    err_markers_ac = err_mean_ac_full[:, 8]
+    err_force_ac = err_mean_ac_full[:, 9]
 
-matcontent = sio.loadmat("solutions/stats_rt_activation_drivenFalse.mat")
+matcontent = sio.loadmat("solutions/stats_rt_test_activation_drivenFalse.mat")
 err_ex = matcontent["err_tries"]
 err_ex_mhe = err_ex[:-1, :].reshape(-1, nb_try, 10)
 err_ex_full = err_ex[-1, :].reshape(1, 10)
 err_mean_ex = np.mean(err_ex_mhe, axis=1)
-err_std_ex = np.std(err_ex_mhe, axis=1)
+err_std_ex = np.std(err_ac_mhe, axis=1)
 err_mean_ex_full = np.concatenate((err_mean_ex, err_ex_full))
 Nmhe_ex = err_mean_ex_full[:, 0]
-ratio_ex = err_mean_ex_full[:-1, 1]
+ratio_ex = err_mean_ex_full[:, 1]
 time_tot_ex = err_mean_ex_full[:, 2]
 time_ex = err_mean_ex_full[:, 3]
 time_std_ex = err_std_ex[:, 3]
 time_mean_ex = err_mean_ex[:, 3]
+if use_log:
+    err_q_ex = np.log10(err_mean_ex_full[:, 4] * 180 / np.pi)
+    err_dq_ex = np.log10(err_mean_ex_full[:, 5])
+    err_muscles_ex = np.log10(err_mean_ex_full[:, 7])
+    err_markers_ex = np.log10(err_mean_ex_full[:, 8])
+    err_force_ex = np.log10(err_mean_ex_full[:, 9])
+else:
+    err_q_ex = err_mean_ex_full[:, 4] * 180 / np.pi
+    err_dq_ex = err_mean_ex_full[:, 5]
+    err_muscles_ex = err_mean_ex_full[:, 7]
+    err_markers_ex = err_mean_ex_full[:, 8]
+    err_force_ex = np.log10(err_mean_ex_full[:, 9])
 
-# Plot Frequency figure fonction of windows size
-seaborn.set_style("whitegrid")
+
+# Configure plot style
+err_ac = "-x"
+err_ex = "-^"
+err_full_ac = "--"
+err_full_ex = "--"
+lw = 1.5
+lw_err = 1.8
+ms_ac = 6
+ms_ex = 4
+mew = 0.08
+err_lim = ":"
+x_y_label_size = 11
+x_y_ticks = 12
+legend_size = 11
+s = 150
+
+# Plot RMSE of Q, markers, muscle force function of windows size (for activation and excitation driven)
 seaborn.color_palette()
-lw = 3  # Line width
-fig = plt.subplot()
-fig.plot(1 / time_mean_ac, lw=lw, label="activation driven")
-fig.plot(1 / time_mean_ex, lw=lw, label="excitation driven")
-fig.plot(
-    np.arange(len(Nmhe_ac) - 1), np.tile(1 / 0.075, (len(Nmhe_ac) - 1, 1)), "--", lw=lw, label="biofeedback standard"
+grid_line_style = "--"
+
+fig = plt.figure()
+plt.gcf().subplots_adjust(left=0.06, bottom=-0.80, right=0.99, top=0.90, wspace=0.08, hspace=0.25)
+
+fig = plt.subplot(421)
+plt.title("q")
+plt.grid(axis="x", linestyle=grid_line_style)
+plt.plot(err_q_ac[:-1], err_ac, lw=lw, ms=ms_ac, label="mhe act. driven")
+plt.plot(err_q_ex[:-1], err_ex, lw=lw, ms=ms_ac, mew=mew, label="mhe excit. driven")
+plt.gca().set_prop_cycle(None)
+plt.scatter(4, err_q_ex[4], s=s, facecolors="none", edgecolors="r")
+plt.plot(
+    np.arange(len(Nmhe_ac) - 1),
+    np.tile(err_q_ac[-1], (len(Nmhe_ac) - 1, 1)),
+    err_full_ac,
+    label="full window act. driven",
 )
-fig.fill_between(
-    range(len(time_mean_ac)), 1 / (time_mean_ac + time_std_ac), 1 / (time_mean_ac - time_std_ac), alpha=0.2
+plt.plot(
+    np.arange(len(Nmhe_ac) - 1),
+    np.tile(err_q_ex[-1], (len(Nmhe_ac) - 1, 1)),
+    err_full_ex,
+    label="full window excit. driven",
 )
-fig.fill_between(
-    range(len(time_mean_ex)), 1 / (time_mean_ex + time_std_ex), 1 / (time_mean_ex - time_std_ex), alpha=0.2
-)
+
 fig.set_xticks(range(len(Nmhe_ac) - 1))
-plt.xticks(fontsize=12)
-plt.yticks(fontsize=12)
+fig.set_xticklabels([])
+plt.ylabel("Logarithmic error", fontsize=x_y_label_size)
+plt.yticks(fontsize=x_y_ticks)
+plt.gca().yaxis.set_major_formatter(ticker.StrMethodFormatter("{x:,.1f}"))
+plt.gca().yaxis.set_major_locator(ticker.MultipleLocator(0.5))
+
+fig = plt.subplot(423)
+plt.title("Muscles force")
+plt.grid(axis="x", linestyle=grid_line_style)
+plt.plot(err_force_ac[:-1], err_ac, lw=lw, ms=ms_ac, label="err. mhe activation driven")
+plt.plot(err_force_ex[:-1], err_ex, lw=lw, ms=ms_ac, mew=mew, label="err. mhe excitation driven")
+plt.gca().set_prop_cycle(None)
+plt.plot(
+    np.arange(len(Nmhe_ac) - 1),
+    np.tile(err_force_ac[-1], (len(Nmhe_ac) - 1, 1)),
+    err_full_ac,
+    label="err. full window activation driven",
+)
+plt.plot(
+    np.arange(len(Nmhe_ac) - 1),
+    np.tile(err_force_ex[-1], (len(Nmhe_ac) - 1, 1)),
+    err_full_ex,
+    label="err. full window excitation driven",
+)
+plt.scatter(4, err_force_ex[4], s=s, facecolors="none", edgecolors="r", label="selected size window")
+
+fig.set_xticks(range(len(Nmhe_ac) - 1))
 fig.set_xticklabels(range(int(Nmhe_ac[0]), int(Nmhe_ac[-2] + 1)))
-plt.legend(fontsize=14)
-plt.ylabel("Freq. (Hz)", fontsize=14)
-plt.xlabel("Size of MHE window", fontsize=14)
+plt.yticks(fontsize=x_y_ticks)
+plt.ylabel("Logarithmic error", fontsize=x_y_label_size)
+plt.xlabel("Size of MHE window", fontsize=x_y_label_size)
+plt.xticks(fontsize=x_y_ticks)
+plt.legend(ncol=1, bbox_to_anchor=(1.05, 1), loc="upper left", borderaxespad=0.0, fontsize=legend_size, frameon=False)
+plt.gca().yaxis.set_major_formatter(ticker.StrMethodFormatter("{x:,.1f}"))
+
+fig = plt.subplot(422)
+plt.title("Markers")
+plt.grid(axis="x", linestyle=grid_line_style)
+plt.plot(err_markers_ac[:-1], err_ac, lw=lw, ms=ms_ac, label="err. mhe activation driven")
+plt.plot(err_markers_ex[:-1], err_ex, lw=lw, ms=ms_ac, mew=mew, label="err. mhe excitation driven")
+plt.gca().set_prop_cycle(None)
+plt.scatter(4, err_markers_ex[4], s=s, facecolors="none", edgecolors="r")
+plt.plot(
+    np.arange(len(Nmhe_ac) - 1),
+    np.tile(err_markers_ac[-1], (len(Nmhe_ac) - 1, 1)),
+    err_full_ac,
+    label="err. full window activation driven",
+)
+plt.plot(
+    np.arange(len(Nmhe_ac) - 1),
+    np.tile(err_markers_ex[-1], (len(Nmhe_ac) - 1, 1)),
+    err_full_ex,
+    label="err. full window excitatiojn driven",
+)
+
+fig.set_xticks(range(len(Nmhe_ac) - 1))
+fig.set_xticklabels(range(int(Nmhe_ac[0]), int(Nmhe_ac[-2] + 1)))
+plt.xlabel("Size of MHE window", fontsize=x_y_label_size)
+plt.xticks(fontsize=x_y_ticks)
+plt.yticks(fontsize=x_y_ticks)
+plt.gca().yaxis.set_major_formatter(ticker.StrMethodFormatter("{x:,.1f}"))
 plt.show()
